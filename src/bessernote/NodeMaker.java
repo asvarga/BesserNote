0ef96ee2fb3a0cf62bd08427e16163d0df90043f
asvarga <alexander_varga@brown.edu>
2014-04-04 04:51:16 -0400

    preliminary NodeMaker popup

---
+++ src/bessernote/NodeMaker.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bessernote;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.stage.Popup;
+
+/**
+ *
+ * @author avarga
+ */
+public class NodeMaker extends Popup {
+    
+    NodeMaker() {
+        setAutoFix(false);
+        setHideOnEscape(true);
+        
+        setWidth(200);
+        setHeight(300);
+        
+        Rectangle r = new Rectangle(0, 0, getWidth(), getHeight()); 
+        r.setFill(Color.GREY);
+        
+        Text t = new Text(10, 20, "Node Creation GUI\nAdd shit here.\nPress Escape.");
+        
+        getContent().addAll(r, t);
+    }
+    
+}
