86dfcb574e2b5ce64e47a4fab6df8546b3166a2d
daviddliu <David_Liu@brown.edu>
2014-04-15 01:22:49 -0400

    YOLO

--- src/bessernote/BesserNote.java
+++ src/bessernote/BesserNote.java
@@ -6,21 +6,24 @@
 package bessernote;
 
 import bessernote.nodemaker.NodeGUI;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.animation.FillTransition; 
-import javafx.application.Application; 
-import javafx.scene.Group; 
-import javafx.scene.Scene; 
-import javafx.scene.paint.Color; 
-import javafx.scene.shape.Rectangle; 
-import javafx.stage.Stage; 
-import javafx.animation.Timeline; 
 import javafx.animation.ParallelTransition; 
 import javafx.animation.RotateTransition; 
 import javafx.animation.ScaleTransition; 
+import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition; 
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
@@ -29,8 +32,13 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.FileChooser;
 import javafx.stage.Popup;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 import javafx.util.Duration; 
 
 /**
@@ -46,6 +54,8 @@ public class BesserNote extends Application {
     
     private Popup popup;
     private NodeGUI nodeGUI;
+    private Window primaryStage;
+    private Desktop desktop = Desktop.getDesktop();
 
     @Override 
     public void start(final Stage stage) { 
@@ -78,16 +88,47 @@ public class BesserNote extends Application {
         //// MENU BAR ////
         
         menuBar = new MenuBar();
+        
+        final FileChooser fileChooser = new FileChooser();
  
         // --- Menu File
         Menu menuFile = new Menu("File");
         MenuItem menuItemNew = new MenuItem("New...");
+        menuItemNew.setOnAction(new EventHandler<ActionEvent> () {
+            @Override
+            public void handle(final ActionEvent e){
+                Application app2 = new BesserNote();
+                Stage anotherStage = new Stage();
+                try {
+                    app2.start(anotherStage);
+                } catch (Exception ex) {
+                    Logger.getLogger(BesserNote.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+        
         MenuItem menuItemOpen = new MenuItem("Open...");
+        menuItemOpen.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(final ActionEvent e) {
+                    File file = fileChooser.showOpenDialog(stage);
+                    if (file != null) {
+                        openFile(file);
+                    }
+                }
+        });
         MenuItem menuItemExit = new MenuItem("Exit");
         menuItemExit.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent t) {
                 System.exit(0);
+            }
+        });
+        MenuItem menuItemSave = new MenuItem("Save");
+        menuItemSave.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent t){
+                
             }
         });
         menuFile.getItems().addAll(
@@ -119,7 +160,49 @@ public class BesserNote extends Application {
         stage.setScene(scene); 
         stage.show(); 
     } 
+    
+        private void openFile(File file) {
+        try {
+            desktop.open(file);
+        } catch (IOException ex) {
+            Logger.getLogger(
+                BesserNote.class.getName()).log(
+                    Level.SEVERE, null, ex
+                );
+        }
+        }
+        /*
+        private void SaveFile(String content, File file){
+        try {
+            FileWriter fileWriter = null;
 
+            fileWriter = new FileWriter(file);
+            fileWriter.write(content);
+            fileWriter.close();
+        } catch (IOException ex) {
+            Logger.getLogger(BesserNote.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        }
+    
+    
+              public void handle(ActionEvent event) {
+              FileChooser fileChooser = new FileChooser();
+  
+              //Set extension filter
+              FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+              fileChooser.getExtensionFilters().add(extFilter);
+              
+              //Show save file dialog
+              File file = fileChooser.showSaveDialog(primaryStage);
+              
+              if(file != null){
+                  SaveFile(Santa_Claus_Is_Coming_To_Town, file);
+              }
+          }
+      });
+        */
+      
     public static void main(String[] args) {
         launch(args);
     }
